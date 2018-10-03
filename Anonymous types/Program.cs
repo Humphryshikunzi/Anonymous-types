0768b1f09766cbe6f1731e02364f876c078540f7
HumphryShikunzi <humphryshikunzi9@gmail.com>
2018-10-03 17:28:19 +0300

    Add project files.

---
+++ Anonymous types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anonymous_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var mycar = new {color = "Black", Brand = "BMW", speed = 180};
+            Console.WriteLine("My car has {0} colour {1} as brand and runs at {2} km/hr");
+            List<int> allnumbers=new List<int>{1,2,4,5,6,7};
+            List<int> evennumbers = allnumbers.FindAll(x => (x % 2) == 0);
+            foreach (var VARIABLE in evennumbers)
+            {
+                Console.WriteLine(VARIABLE);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
